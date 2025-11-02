457e360a69304ff80d88c3a40bcf5fb231bb7eb6
Adriano Demartin <adriano.demartinez@gmail.com>
2025-11-01 21:15:58 -0600

    Initial Commit

---
+++ PalabraDD/PalabraDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalabraDDApp.swift
+//  PalabraDD
+//
+//  Created by Developer on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct PalabraDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
